571a8722cba57996e555eaf24025a00de9ffdcc9
nooblyk <nooblyk@gmail.com>
2019-02-10 17:08:32 +0200

    first commit

---
+++ exp11/script.js
@@ -0,0 +1,22 @@
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+    appData = {
+        budget: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: false
+    };
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    a2 = prompt("“Во сколько обойдется?”", ""),
+    b1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    b2 = prompt("Во сколько обойдется?", "");
+
+
+appData.expenses.a1 = a2;
+appData.expenses.b1 = b2;
+
+
+alert(appData.budget / 30);
